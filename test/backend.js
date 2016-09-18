9eefe9b34de187b0fd8a5affd46ed7884e9dbe37
Sudharshan Ravindran <suddir@gmail.com>
2016-09-18 15:10:28 +0800

    Corrected unit tests

--- test/backend.js
+++ test/backend.js
@@ -2,13 +2,13 @@
 
 const assert = require('assert');
 
-const backend_config = require('../backend');
+const config = require('../backend');
 const lint = require('./utils/lint');
 
 describe('Testing backend eslint configuration', function () {
     it('CASE 1: no errors', function () {
         const code = "'use strict';\n\nmodule.exports = function () {\n    return 42;\n};\n";
-        const errors = lint(code, backend_config);
+        const errors = lint(code, config);
 
         assert.equal(errors.length, 0);
     });

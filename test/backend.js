a215182aa1ce2104960b390e5ed6269dabb58fd5
Sudharshan Ravindran <suddir@gmail.com>
2016-09-24 12:05:48 +0800

    Corrected for coding guideline

--- test/backend.js
+++ test/backend.js
@@ -7,7 +7,7 @@ const lint = require('./utils/lint');
 
 describe('Testing backend eslint configuration', function () {
     it('CASE 1: no errors', function () {
-        const code = "'use strict';\n\nmodule.exports = function () {\n    return 42;\n};\n";
+        const code = '\'use strict\';\n\nmodule.exports = function () {\n    return 42;\n};\n';
         const errors = lint(code, config);
 
         assert.equal(errors.length, 0);
